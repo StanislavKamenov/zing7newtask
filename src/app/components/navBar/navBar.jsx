3c2b07299cc893cb66f5747922b2c12e0abd60c1
AstrO <kostadinov226@abv.bg>
2024-04-22 22:59:12 +0300

    litle changes

--- src/app/components/navBar/navBar.jsx
+++ src/app/components/navBar/navBar.jsx
@@ -3,50 +3,51 @@ import { useState } from "react"
 import Image from "next/image"
 import Logo from "../../../../public/Logo.png"
 import { CiMenuBurger } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function navBar() {
     
     const [isNav, setIsNav] = useState(false);
 
-    function HandleClick() {
+    function handleClick() {
         setIsNav(prev => !prev)
     }
 
 
     return (
-        <header className="max-w-[1440px] max-h-[10vh] lg:flex-row lg:justify-between">
+        <header className= "max-w-[1440px] max-h-[10vh] lg:flex-row lg:justify-between">
             <nav>
                 <ul>
                     <div className="space-x-10 mt-[-2rem] mr-[25rem] font-semibold">
-                        <a href="">Product</a>
-                        <a href="">Pricing</a>
-                        <a href="">Company</a>
-                        <a href="">Resources</a>
-                        <a href="">Contact</a>
+                        <a href="" className="transition duration-500 hover:text-blue-900">Product</a>
+                        <a href="" className="transition duration-500 hover:text-blue-900">Pricing</a>
+                        <a href="" className="transition duration-500 hover:text-blue-900">Company</a>
+                        <a href="" className="transition duration-500 hover:text-blue-900">Resources</a>
+                        <a href="" className="transition duration-500 hover:text-blue-900">Contact</a>
                     </div>
                 </ul>
-                <div className="space-x-5 fixed right-0 mt-[-2rem] mr-[10rem]">
-                    <button>Log In</button>
-                    <button>Try For Free</button>
+                <div className="space-x-16 fixed right-0 mt-[-2rem] mr-[10rem]">
+                    <button className="font-extrabold transition duration-500 hover:text-blue-900">Log In</button>
+                    <button className="bg-custom-blue text-white w-[113px] h-[33px] rounded-tr-2xl font-extrabold transition duration-500 hover:text-teal-100">Try For Free</button>
                 </div>
                 <div className="fixed left-[calc(15%-0.5px)] top-14 transform translate-x-[-50%] translate-y-[10%]">
-                    <Image src={Logo} alt="Logo" width={95} height={28} />
+                    <Image src={Logo} alt="Logo" width={95} height={28} className="cursor-pointer" />
                 </div>
+                <CiMenuBurger onClick={handleClick} className="max-lg:flex lg:hidden visible text-black absolute left-4 top-4" size={32} />
             </nav>
 
             {/* MobileNav */}
-            
             {isNav && 
              <div className="bg-black/80 fixed w-full h-full z-10 top-0 left-0"> 
+             <AiOutlineClose onClick={handleClick} className="max-lg:flex lg:hidden visible text-black absolute ml-[14rem] top-0" size={32} />
                 <div className="bg-white w-64 h-full" >
-                <CiMenuBurger onClick={HandleClick} className="text-black" size={32}/>
-                <ul className="text-xl flex flex-col p-4">
+                    <ul className="text-xl flex flex-col p-4">
                         <a href="">Product</a>
                         <a href="">Pricing</a>
                         <a href="">Company</a>
                         <a href="">Resources</a>
                         <a href="">Contact</a>
-                        </ul>
+                    </ul>
                 </div>
             </div>}
         </header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,17 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        'max-lg': {'max': '1330px'}, // Custom breakpoint at 1330px
+      },
+      colors: {
+        "custom-blue": "#150050",
+      }
     },
   },
   plugins: [],
 };
+
 export default config;
