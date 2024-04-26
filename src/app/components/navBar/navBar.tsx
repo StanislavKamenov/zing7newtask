fe6ce297c819695ce82ee9df3b14333b92a6af46
AstrO <kostadinov226@abv.bg>
2024-04-26 13:55:33 +0300

    Start maing responsive on Hero sections fixing menu and little changes

--- src/app/components/navBar/navBar.tsx
+++ src/app/components/navBar/navBar.tsx
@@ -14,11 +14,11 @@ export default function NavBar() {
     }
 
     return (
-        <header className="max-w-[1440px] max-h-[10vh] lg:flex-row lg:justify-between">
+        <header className="max-w-[1440px] max-h-[10vh] xl:flex-row xl:justify-between">
             {/* Desktop Nav */}
-            <nav className="hidden lg:flex">
+            <nav className="hidden xl:flex">
                 <ul>
-                    <div className="space-x-10 mt-[-2rem] mr-[25rem] font-semibold">
+                    <div className="fixed space-x-10 mt-[-2rem] ml-[-30rem] font-semibold">
                         <a href="" className="transition duration-500 hover:text-blue-900">Product</a>
                         <a href="" className="transition duration-500 hover:text-blue-900">Pricing</a>
                         <a href="" className="transition duration-500 hover:text-blue-900">Company</a>
@@ -26,7 +26,7 @@ export default function NavBar() {
                         <a href="" className="transition duration-500 hover:text-blue-900">Contact</a>
                     </div>
                 </ul>
-                <div className="space-x-10 fixed right-0 mt-[-2rem] mr-[20rem]">
+                <div className="space-x-10 fixed right-0 mt-[-3rem] mr-[20rem]">
                     <button className="font-extrabold transition duration-500 hover:text-blue-900">Log In</button>
                     <button className="bg-custom-blue text-white w-[142px] h-[56px] border rounded-2xl font-extrabold transition duration-500 hover:text-teal-100">Try For Free</button>
                 </div>
@@ -36,10 +36,10 @@ export default function NavBar() {
             </nav>
 
             {/* Mobile Nav */}
-            <nav className="lg:hidden">
+            <nav className="xl:hidden">
                 <CiMenuBurger onClick={handleClick} className="visible text-black absolute left-4 top-4" size={32} />
                 <div className="visible text-black absolute space-x-1 left-12 top-5">
-                    <Image src={Logo} alt="Logo" width={95} height={28} className="cursor-pointer lg:justify-between" />
+                    <Image src={Logo} alt="Logo" width={95} height={28} className="cursor-pointer xl:justify-between" />
                 </div>
                 <div className="space-x-16 fixed right-10 mt-[-5rem] mr-[0rem]">
                     <button className="bg-custom-blue text-white w-[113px] h-[33px] border rounded-2xl font-extrabold transition duration-500 hover:text-teal-100">Try For Free</button>
@@ -48,8 +48,8 @@ export default function NavBar() {
 
             {/* MobileNav */}
             {isNav && 
-    <div className="bg-black/80 fixed w-full h-full z-10 top-0 lg:hidden left-0 flex justify-center items-center animate-fadeIn"> 
-        <AiOutlineClose onClick={handleClick} className="max-lg:flex lg:hidden visible text-black absolute ml-[14rem] top-0" size={32} />
+    <div className="bg-black/80 fixed w-full h-full z-10 top-0 xl:hidden left-0 flex justify-center items-center animate-fadeIn"> 
+        <AiOutlineClose onClick={handleClick} className="max-xl:flex xl:hidden visible text-black absolute ml-[14rem] top-0" size={32} />
         <div className="bg-white p-4 rounded-md w-full h-full flex justify-center items-center">
             <ul className="text-2xl flex flex-col">
                 <a href="" className="my-2">Product</a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Navbar from './components/navBar/navBar'
-import Info from "./components/Info/info";
+import Hero from "./components/hero/hero";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <Navbar />
-     <Info />
+     <Hero />
     </main>
   );
 }
